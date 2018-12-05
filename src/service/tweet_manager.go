af12fb7c8b0a5679218fabb40d13c5d8ffed192a
Franco Ferrari <franco.ferrari@mercadolibre.com>
2018-12-05 15:44:33 -0300

    Añade tests para checkear que los tweets tengan que tener usuario, texto, y que ese texto no pueda tener mas de 140 caracteres

--- src/service/tweet_manager.go
+++ src/service/tweet_manager.go
@@ -1,6 +1,10 @@
 package service
 
-import "github.com/franferrari/Twitter/src/domain"
+import (
+	"fmt"
+
+	"github.com/franferrari/Twitter/src/domain"
+)
 
 //Tweet es el string con el comentario
 var Tweet *domain.Tweet
@@ -10,8 +14,18 @@ func main() {
 }
 
 //PublishTweet publica un tweet
-func PublishTweet(tweet *domain.Tweet) {
+func PublishTweet(tweet *domain.Tweet) error {
+	if tweet.User == "" {
+		return fmt.Errorf("User is required")
+	}
+	if tweet.Text == "" {
+		return fmt.Errorf("Text is required")
+	}
+	if len(tweet.Text) >= 140 {
+		return fmt.Errorf("Tweet can't exceed 140 characters")
+	}
 	Tweet = tweet
+	return nil
 }
 
 //GetTweet obtiene un tweet
--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -8,18 +8,62 @@ import (
 )
 
 func TestPublishedTweetIsSaved(t *testing.T) {
+	var tweet *domain.Tweet
 	user := "grupoesfera"
 	text := "This is my first tweet"
-	tweet := domain.NewTweet(user, text)
+	tweet = domain.NewTweet(user, text)
 
 	service.PublishTweet(tweet)
 
 	publishedTweet := service.GetTweet()
+
 	if publishedTweet.User != user && publishedTweet.Text != text {
 		t.Errorf("Expected tweet is %s: %s \n But it's %s: %s", user, tweet, publishedTweet.User, publishedTweet.Text)
 	}
 
 	if publishedTweet.Date == nil {
-		t.Error("Expected tweet can't be nil")
+		t.Errorf("Expected tweet can't be nil")
+	}
+}
+
+func TestTweetWithoutUserIsNotPublished(t *testing.T) {
+	var tweet *domain.Tweet
+	var user string
+	text := "This is my first tweet"
+	tweet = domain.NewTweet(user, text)
+
+	var err error
+	err = service.PublishTweet(tweet)
+
+	if err != nil && err.Error() != "User is required" {
+		t.Error("Expected error is user is required")
+	}
+}
+
+func TestTweetWithoutTextIsNotPublished(t *testing.T) {
+	var tweet *domain.Tweet
+	user := "usuario"
+	var text string
+	tweet = domain.NewTweet(user, text)
+
+	var err error
+	err = service.PublishTweet(tweet)
+
+	if err != nil && err.Error() != "Text is required" {
+		t.Error("Expected error is text is required")
+	}
+}
+
+func TestTweetExceeding140CharactersIsNotPublished(t *testing.T) {
+	var tweet *domain.Tweet
+	user := "usuario"
+	text := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	tweet = domain.NewTweet(user, text)
+
+	var err error
+	err = service.PublishTweet(tweet)
+
+	if err != nil && err.Error() != "Tweet can't exceed 140 characters" {
+		t.Error("Expected error is Tweet can't exceed 140 character")
 	}
 }
